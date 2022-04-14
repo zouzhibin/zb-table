f34818363c8ba09d52424cea4c7c252383f475d9
zouzhibing <zouzhibing@anthead.cn>
2022-04-14 09:35:26 +0800

    增加分页

--- pages/pagination/pagination.vue
+++ pages/pagination/pagination.vue
@@ -1,16 +1,40 @@
 <template>
 	<view class="zb-pagination">
-    <zb-pagination
-        :current-page="2"
-        :total="5" @current-change="handleCurrentChange"/>
+    <uni-card title="全部功能" >
+      <zb-pagination
+          :current-page="2"
+          :total="45"
+          layout="first,prev,next,last,total"
+          :page-size="10"
+          @current-change="handleCurrentChange"/>
+    </uni-card>
+
+    <uni-card title="简单功能" >
+      <zb-pagination
+          :current-page="2"
+          :total="45"
+          layout="prev,next"
+          :page-size="10"
+          @current-change="handleCurrentChange"/>
+    </uni-card>
+
+    <uni-card title="总条数" >
+      <zb-pagination
+          :current-page="2"
+          :total="45"
+          layout="total,prev,next"
+          :page-size="10"
+          @current-change="handleCurrentChange"/>
+    </uni-card>
+
 	</view>
 </template>
 
 <script>
-import ZbPagination from '../../components/pagination/pagination'
+// import ZbPagination from '../../components/pagination/pagination'
 	export default {
     components:{
-      ZbPagination
+      // ZbPagination
     },
 		data() {
 			return {
@@ -19,6 +43,11 @@ import ZbPagination from '../../components/pagination/pagination'
 		},
 		methods: {
       handleCurrentChange(val){
+        uni.showToast({
+          icon:'none',
+          duration:3000,
+          title:`当前页: ${val}`
+        })
         console.log(`当前页: ${val}`);
       }
 		}
@@ -26,5 +55,17 @@ import ZbPagination from '../../components/pagination/pagination'
 </script>
 
 <style lang="scss" scoped>
-
+::v-deep{
+  .uni-card{
+    margin: 0px!important;
+    padding: 8px!important;
+    .uni-card__content{
+      padding: 0!important;
+      padding-top: 8px!important;
+    }
+  }
+  .uni-card__header{
+    padding-left: 0!important;
+  }
+}
 </style>
